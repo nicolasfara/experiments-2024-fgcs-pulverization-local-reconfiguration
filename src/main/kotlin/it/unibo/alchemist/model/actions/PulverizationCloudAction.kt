15b8b2c3a7fe03660c969a78415910a4cc9734a4
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-05-15 18:13:08 +0200

    feat: implement cloud costs

--- src/main/kotlin/it/unibo/alchemist/model/actions/PulverizationCloudAction.kt
+++ src/main/kotlin/it/unibo/alchemist/model/actions/PulverizationCloudAction.kt
@@ -10,16 +10,19 @@ import it.unibo.alchemist.utils.molecule
 class PulverizationCloudAction<T>(
     private val environment: Environment<T, *>,
     private val node: Node<T>,
+    private val cloudCost: Double,
 ) : AbstractLocalAction<T>(node) {
     private val CloudActiveComponents by molecule()
     private val CloudComponentsTime by molecule()
     private val CloudPower by molecule()
+    private val CloudCost by molecule()
 
     private val cloudConsumptionModel: CloudConsumptionModel<*> by lazy {
         node.properties.filterIsInstance<CloudConsumptionModel<*>>().first()
     }
     private var componentsExecutionTime = 0.0
     private var lastUpdate = 0.0
+    private var actualCloudCost = 0.0
 
     override fun cloneAction(node: Node<T>?, reaction: Reaction<T>?): Action<T> {
         TODO("Not yet implemented")
@@ -34,6 +37,9 @@ class PulverizationCloudAction<T>(
 
         val cloudConsumption = cloudConsumptionModel.getConsumptionSinceLastUpdate(currentTime)
         node.setConcentration(CloudPower, (cloudConsumption * delta) as T)
+
+        actualCloudCost += (cloudCost / 3600.0) * delta * cloudConsumption
+        node.setConcentration(CloudCost, actualCloudCost as T)
 
         componentsExecutionTime += delta * cloudConsumptionModel.getActiveComponents().count()
         node.setConcentration(CloudComponentsTime, componentsExecutionTime as T)

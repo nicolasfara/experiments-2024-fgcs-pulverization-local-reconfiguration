3abbd9968fb6d93d1b20aa4192d397a6643607e1
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-05-16 17:44:56 +0200

    feat: implement derivative of travel distance and cloud costs

--- src/main/kotlin/it/unibo/alchemist/model/actions/PulverizationCloudAction.kt
+++ src/main/kotlin/it/unibo/alchemist/model/actions/PulverizationCloudAction.kt
@@ -18,6 +18,7 @@ class PulverizationCloudAction<T>(
     private val CloudComponentsTime by molecule()
     private val CloudPower by molecule()
     private val CloudCost by molecule()
+    private val CloudCostPerHour by molecule()
     private val CloudInstance by molecule()
 
     private val cloudConsumptionModel: CloudConsumptionModel<*> by lazy {
@@ -26,6 +27,8 @@ class PulverizationCloudAction<T>(
     private var componentsExecutionTime = 0.0
     private var lastUpdate = 0.0
     private var actualCloudCost = 0.0
+    private val timeWindow = 3600.0 // 1 hour
+    private var costSamples: Map<Double, Double> = mapOf()
 
     override fun cloneAction(node: Node<T>?, reaction: Reaction<T>?): Action<T> {
         TODO("Not yet implemented")
@@ -44,8 +47,14 @@ class PulverizationCloudAction<T>(
         val cloudInstances = ceil(cloudConsumption / 220.0) // 220W TDP per cloud instance
         node.setConcentration(CloudInstance, cloudInstances as T)
 
-        actualCloudCost += (cloudCost / 3600) * delta * cloudInstances
+        val instantCost = (cloudCost / 3600) * delta * cloudInstances
+        actualCloudCost += instantCost
         node.setConcentration(CloudCost, actualCloudCost as T)
+
+        costSamples += currentTime to instantCost
+        costSamples = costSamples.filter { it.key > currentTime - timeWindow } // keep only the last hour
+        val costInLastHour = costSamples.values.sum()
+        node.setConcentration(CloudCostPerHour, costInLastHour as T)
 
         componentsExecutionTime += delta * cloudConsumptionModel.getActiveComponents().count()
         node.setConcentration(CloudComponentsTime, componentsExecutionTime as T)
--- src/main/kotlin/it/unibo/alchemist/model/actions/TraveledDistanceAction.kt
+++ src/main/kotlin/it/unibo/alchemist/model/actions/TraveledDistanceAction.kt
@@ -12,8 +12,11 @@ class TraveledDistanceAction<T, P : Position<P>>(
     private val node: Node<T>,
 ) : AbstractLocalAction<T>(node) {
     private val TraveledDistance by molecule()
+    private val TraveledDistanceLastTenMinutes by molecule()
     private var lastPosition: P? = null
     private var traveledDistance = 0.0
+    private val timeWindow = 600.0 // 10 minutes
+    private var traveledDistancesSamples = mapOf<Double, Double>()
 
     override fun cloneAction(node: Node<T>?, reaction: Reaction<T>?): Action<T> {
         TODO("Not yet implemented")
@@ -22,9 +25,14 @@ class TraveledDistanceAction<T, P : Position<P>>(
     @Suppress("UNCHECKED_CAST")
     override fun execute() {
         val currentPosition = environment.getPosition(node)
+        val currentTime = environment.simulation.time.toDouble()
         lastPosition?.let {
-            traveledDistance += currentPosition.distanceTo(it)
+            val traveledDistanceSample = currentPosition.distanceTo(it)
+            traveledDistance += traveledDistanceSample
             node.setConcentration(TraveledDistance, traveledDistance as T)
+            traveledDistancesSamples += currentTime to traveledDistanceSample
+            traveledDistancesSamples = traveledDistancesSamples.filter { s -> s.key > currentTime - timeWindow }
+            node.setConcentration(TraveledDistanceLastTenMinutes, traveledDistancesSamples.values.sum() as T)
         }
         lastPosition = currentPosition
     }

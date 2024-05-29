e3c9285e00a80a8c106851cb2d3e4a85876529a1
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-05-29 16:48:53 +0200

    build: add gui effects depenmdencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,9 +41,9 @@ multiJvm {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(libs.bundles.alchemist.protelis)
-    if (!GraphicsEnvironment.isHeadless()) {
-        implementation("it.unibo.alchemist:alchemist-swingui:${libs.versions.alchemist.get()}")
-    }
+//    if (!GraphicsEnvironment.isHeadless()) {
+    implementation("it.unibo.alchemist:alchemist-swingui:${libs.versions.alchemist.get()}")
+//    }
 }
 
 // Heap size estimation for batches
